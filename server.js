2f7f9bac588cc874ea07dfd43d68b88a00bddc2a
augustinemmaphet0/ <augustinemmapheto@gmail.com>
2025-04-21 21:01:47 +0000

    server initital c9mmi

---
+++ server.js
@@ -0,0 +1,14 @@
+const movies = [
+      { id: 1, title: "Inception", director: "Christopher Nolan" },
+        { id: 2, title: "Interstellar", director: "Christopher Nolan" }
+        ];
+
+const series = [
+          { id: 1, title: "Breaking Bad", seasons: 5 },
+            { id: 2, title: "Stranger Things", seasons: 4 }
+            ];
+
+const songs = [
+              { id: 1, title: "Blinding Lights", artist: "The Weeknd" },
+                { id: 2, title: "Shape of You", artist: "Ed Sheeran" }
+                ];
